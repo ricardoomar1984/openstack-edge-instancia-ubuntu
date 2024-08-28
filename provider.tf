cdf00104059e60e805c00913b7cdb362c047e45b
ricardo.omar <ricardo.omar@gmail.com>
2024-08-28 10:50:07 -0300

    second commit

--- provider.tf
+++ provider.tf
@@ -1,4 +1,4 @@
-terraform {
+/terraform {
   required_providers {
     openstack = {
       source  = "terraform-provider-openstack/openstack"
@@ -8,11 +8,11 @@ terraform {
 }
 
 provider "openstack" {
-  auth_url      =  var.os_auth_url    #"https://identity.spo1.edge.embratel.cloud:5000/v3"
-  user_name     =  var.os_username    #"terraform"                                        
-  password      =  var.os_password    #"P@ssw0rd01"                                       
-  tenant_name   =  var.os_tenant_name #"1279234-project"                                  
-  domain_name   =  var.os_domain      #"1279234-domain"                                   
-  region        =  var.os_region      #"spo1"                                             
+  auth_url      =  var.os_auth_url    
+  user_name     =  var.os_username                                            
+  password      =  var.os_password                                           
+  tenant_name   =  var.os_tenant_name                                   
+  domain_name   =  var.os_domain                                         
+  region        =  var.os_region                                                   
   endpoint_type =  "public"
 }
